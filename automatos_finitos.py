98ce3b00662dc4a4f3346fff144e98ab173619f3
Guilherme <gggbsilvestre@usp.br>
2024-04-09 16:34:08 -0300

    teste

---
+++ automatos_finitos.py
@@ -0,0 +1,11 @@
+def calcular():
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite o segundo número: "))
+
+    resultado = numero1 + numero2
+
+    print("O resultado da operação é:", resultado)
+
+calcular()
+
+#codigo teste
